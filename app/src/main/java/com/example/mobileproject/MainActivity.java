3585c91e62dc8020795460e1627638a2f993568c
Nurin2001 <a182071@siswa.ukm.edu.my>
2023-01-09 04:59:49 +0800

    boleh start buat database untuk profile.. boleh try intent kat contact

--- app/src/main/java/com/example/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.mobileproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                //drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id) {
+                    case  R.id.nav_profile:
+                        startActivity(new Intent(MainActivity.this, Profile.class));
+                        // onStop();
+                        Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        break;
+                    case  R.id.nav_contact:
+                        Toast.makeText(MainActivity.this, "Contact", Toast.LENGTH_SHORT).show();
+                        //onStop();
+                        startActivity(new Intent(MainActivity.this, Contact.class));
+                        break;
+                    case  R.id.nav_logout:
+                        AlertDialog.Builder logoutdialog = new AlertDialog.Builder(navigationView.getContext());
+                        logoutdialog.setTitle("Logout").setMessage("Are you sure you want to logout?")
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        FirebaseAuth.getInstance().signOut();
+                                        startActivity(new Intent(MainActivity.this, Login.class));
+                                        //onStop();
+                                        finish();
+                                    }
+                                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                    }
+                                });
+                        logoutdialog.create().show();
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
     }
 }
